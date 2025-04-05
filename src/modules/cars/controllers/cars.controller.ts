f7b2839010f46af90420e3b2b74334d2c2a46890
Nathalie Castillo <castillowilmara3@gmail.com>
2025-04-04 22:19:37 -0600

    eliminar con marca

--- src/modules/cars/controllers/cars.controller.ts
+++ src/modules/cars/controllers/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Delete } from '@nestjs/common';
 import { CarsService } from '../service/cars.service';
 import { CreateCarDto } from '../dto/car.dto';
 
@@ -7,12 +7,22 @@ export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
   @Get()
-  getCarsAll() {
-    return 'Todos los Carros';
+  findAll() {
+    return this.carsService.findAll();
   }
 
   @Post()
-  createCar(@Body() CreateCarDto) {
-    return this.carsService.create(CreateCarDto);
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return this.carsService.create(createCarDto);
+  }
+
+  @Get(':id')
+  finOne(@Param('id') id: number) {
+    return this.carsService.findOne(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.carsService.remove(id);
   }
 }
